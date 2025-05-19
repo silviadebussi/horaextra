314470c5c2ae23dd14e2fba9344f5e6266e01ba5
silvia debussi <silviadebussi2@gmail.com>
2025-05-19 10:41:03 -0300

    templates configurado + sqllite

--- config/settings.py
+++ config/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nome_do_banco',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
--- core/views.py
+++ core/views.py
@@ -13,20 +13,21 @@ class IndexView(TemplateView):
 
 
 
-    @login_required
-    def lista_registros(request):
-        registros = RegistroHora.objects.filter(usuario=request.user).order_by('-data')
-        return render(request, 'core/lista_registros.html', {'registros': registros})
 
-    @login_required
-    def criar_registro(request):
-        if request.method == 'POST':
-            form = RegistroHoraForm(request.POST)
-            if form.is_valid():
-                registro = form.save(commit=False)
-                registro.usuario = request.user
-                registro.save()
-                return redirect('lista_registros')
-        else:
-            form = RegistroHoraForm()
-        return render(request, 'core/criar_registro.html', {'form': form})
+@login_required
+def lista_registros(request):
+    registros = RegistroHora.objects.all()
+    return render(request, 'core/lista_registros.html', {'registros': registros})
+
+@login_required
+def criar_registro(request):
+    if request.method == 'POST':
+        form = RegistroHoraForm(request.POST)
+        if form.is_valid():
+            registro = form.save(commit=False)
+            registro.usuario = request.user
+            registro.save()
+            return redirect('lista_registros')
+    else:
+        form = RegistroHoraForm()
+    return render(request, 'core/criar_registro.html', {'form': form})
